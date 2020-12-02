8053f4c150a2c84b5b0123a74686ca34ccde9891
harithzulfaizal <56865880+harithzulfaizal@users.noreply.github.com>
2020-12-02 17:52:04 +0800

    Update day2.py

--- day2.py
+++ day2.py
@@ -7,40 +7,26 @@ print(test_input.iloc[0,2])
 total = len(test_input)
 print(total)
 #part1
-'''
-numlegit_password = 0
+
+validpass_part1 = 0
+validpass_part2 = 0
+
 for i in range(total):
     password = test_input.iloc[i,2]
     limit = test_input.iloc[i,0].split('-')
     limit = [int(x) for x in limit] #0 min 1 max
+    position = limit
 
     letter = test_input.iloc[i,1].replace(':','')
 
     counter = password.count(letter) #min<counter<max
 
     if counter>= limit[0] and counter<=limit[1]:
-        numlegit_password+=1
+        validpass_part1+=1
 
-print(numlegit_password)
-'''
-
-#part2
-numlegit_password = 0
-for i in range(total):
-    #no index zero, so the index of the password should be +1 or position -1
-    password = test_input.iloc[i,2]
-    position = test_input.iloc[i,0].split('-')
-    position = [int(x) for x in position] #0 min 1 max
-
-    letter = test_input.iloc[i,1].replace(':','')
-
-    #ONLY one of the letter, two -invalid
     if letter == password[position[0]-1] or letter == password[position[1]-1]:
         if password[position[0]-1] != password[position[1]-1]:
             print(letter, password[position[0]-1], password[position[1]-1])
-            numlegit_password+=1
+            validpass_part2+=1
 
-print(numlegit_password)
-
-
-
+print('Part 1:', validpass_part1, 'Part 2:', validpass_part2)
