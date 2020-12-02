98d1168639c1e512f4eb64ba2ab87cda5b9583bc
harithzulfaizal <56865880+harithzulfaizal@users.noreply.github.com>
2020-12-02 21:35:50 +0800

    Update day1.py

--- day1.py
+++ day1.py
@@ -3,12 +3,15 @@ import pandas as pd
 listyr = pd.read_csv("list_adventday1.csv", header=None)
 
 years = list(listyr.iloc[:,0])
-product = []
+product1 = []
+product2 = []
 
 for i, yr in enumerate(years):
     for j, yr2 in enumerate(years[i:]):
+            if yr+yr2 == 2020:
+                product1.append(yr*yr2)
         for k, yr3 in enumerate(years[j:]):
             if yr+yr2+yr3 == 2020:
-                product.append(yr*yr2*yr3)
+                product2.append(yr*yr2*yr3)
 
-print(product)
+print("Task 1:", product1, "Task 2:", product2)
