362c93638ad0f38c840dfd71ac92172a5fbe62f8
harithzulfaizal <56865880+harithzulfaizal@users.noreply.github.com>
2020-12-09 17:22:30 +0800

    Update day8*.py
    Attempt at implementing class (read: scuffed)

--- day8/day8*.py
+++ day8/day8*.py
@@ -1,62 +1,68 @@
-def read_file(input_path):
-    return [i.split(' ') for i in open(input_path).read().splitlines()]
+import copy
 
+class day8:
+    def __init__(self, input_path):
+        self.input = [i.split(' ') for i in open(input_path).read().splitlines()]
 
-def part_one(instr):
-    acc_count = 0
-    j = 0 #start
-    global index_norpt
-    index_norpt = []
+    @property
+    def part_one(self):
+        acc_count = 0
+        instr = self.input
+        j = 0 #start
+        index_norpt = []
+        nj = []
 
-    while j not in index_norpt:
-        index_norpt.append(j)
-        name, amt = instr[j][0], instr[j][1]
-        if name == 'acc':
-            acc_count += int(amt)
-            j += 1
+        while j not in index_norpt:
+            index_norpt.append(j)
             name, amt = instr[j][0], instr[j][1]
-        if name == 'nop':
-            j += 1
-            name, amt = instr[j][0], instr[j][1]
-        if name == 'jmp':
-            j += int(amt)
-            name, amt = instr[j][0], instr[j][1]
+            if name == 'acc':
+                acc_count += int(amt)
+                j += 1
+            if name == 'nop':
+                j += 1
+            if name == 'jmp':
+                j += int(amt)
 
-    return acc_count, instr
+        return acc_count, instr
 
-def count_two(instr):
-    acc_count = 0
-    j = 0
-    local_visited = []
-    while j not in local_visited and j < len(instr):
-        local_visited.append(j)
-        name = instr[j][0]
-        amt = int(instr[j][1])
-        if name == 'acc':
-            acc_count += amt
-            j += 1
-        elif name == 'jmp':
-            j += amt
-        else:
-            j += 1
-    if j not in local_visited:
-        print(acc_count)
+    @property
+    def part_two(self):
+        const_instr = self.input
+        nj = []
+        for k, item in enumerate(const_instr):
+            j = 0
+            acc_count = 0
+            index_norpt = []
+            instr = copy.deepcopy(const_instr)
 
-def part_two(instr):
-    for v in index_norpt[::-1]:
-        changednj = instr
-        if changednj[v][0] == 'jmp':
-            changednj[v][0] = 'nop'
-            count_two(changednj)
-        elif changednj[v][0] == 'nop':
-            changednj[v][0] = 'jmp'
-            count_two(changednj)
+            if k not in nj:
+                if instr[k][0] == 'jmp':
+                    instr[k][0] = 'nop'
+                    nj.append(k)
+                elif instr[k][0] == 'nop':
+                    instr[k][0] = 'jmp'
+                    nj.append(k)
 
+            try:
+                while j not in index_norpt and j < len(instr):
+                    index_norpt.append(j)
+                    name, amt = instr[j][0], instr[j][1]
+                    if name == 'acc':
+                        acc_count += int(amt)
+                        j += 1
+                    if name == 'nop':
+                        j += 1
+                    if name == 'jmp':
+                        j += int(amt)
+                if j not in index_norpt:
+                    return acc_count
+            except IndexError:
+                #print('here')
+                return (acc_count)
 
 if __name__ == "__main__":
     input_path = "day8_input.txt"
-    input = read_file(input_path)
+    input = day8(input_path)
 
-    print(part_one(input)[0])
-    part_two(input)
-
+    print('Task 1:', input.part_one[0])
+    print('Task 2:', input.part_two)
